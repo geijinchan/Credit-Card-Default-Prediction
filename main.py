ca15c5720b38d116b0e269ef89bb9673a7f8de09
geijin <abhishekravikumar24@gmail.com>
2023-07-17 20:40:36 +0530

    Data validation part done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.exception import CustomException
 from src.utils import get_collection_as_dataframe
 from src.entity import config_entity
 from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
 
 if __name__ == "__main__":
     try:
@@ -10,6 +11,12 @@ if __name__ == "__main__":
         data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config)
         print(data_ingestion_config.to_dict())
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-        print(data_ingestion.initiate_data_ingestion())
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config,
+                                         data_ingestion_artifact=data_ingestion_artifact)
+        data_validation_artifatc = data_validation.initiate_data_validation()
+
     except Exception as e:
         raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import sys
+import sys,os
 from src.exception import CustomException
 from src.logger import logging
 from src.config import mongo_client
+import yaml
 
 def get_collection_as_dataframe(database_name: str, collection_name: str) -> pd.DataFrame:
     """
@@ -26,3 +27,21 @@ def get_collection_as_dataframe(database_name: str, collection_name: str) -> pd.
         return df
     except Exception as e:
         raise CustomException(e, sys)
+   
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise e
