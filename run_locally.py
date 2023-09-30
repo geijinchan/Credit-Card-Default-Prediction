a30f7cf4b6d9a5e84c9ef3047858de412e2ce53b
geijin <abhishekravikumar24@gmail.com>
2023-09-30 13:00:26 +0530

    cicd added 2

--- run_locally.py
+++ run_locally.py
@@ -99,5 +99,5 @@ def predict_datapoint():
             raise CustomException(e, sys)
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", debug=True)
+    # app.run(host="0.0.0.0", debug=True)
     app.run(host="0.0.0.0",port = 8080)
